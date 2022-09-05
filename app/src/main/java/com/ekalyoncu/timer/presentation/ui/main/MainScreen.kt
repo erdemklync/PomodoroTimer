d5393da23b2b446b3f59a3d3274b0ff1fc7e30bb
Erdem <ekalyoncu81@gmail.com>
2022-09-05 20:34:27 +0300

    Initial commit

---
+++ app/src/main/java/com/ekalyoncu/timer/presentation/ui/main/MainScreen.kt
@@ -0,0 +1,8 @@
+package com.ekalyoncu.timer.presentation.ui.main
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+
+}
---
+++ app/src/main/java/com/ekalyoncu/timer/presentation/ui/main/components/TimeIndicator.kt
@@ -0,0 +1,8 @@
+package com.ekalyoncu.timer.presentation.ui.main.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TimeIndicator() {
+    
+}
